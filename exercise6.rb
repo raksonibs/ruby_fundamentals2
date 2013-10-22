a53e9ac11afe50a3f3a1035b8a42091b0a4226e7
Oskar Niburski <oskarniburski@gmail.com>
2013-10-22 01:14:29 -0400

    Exercise six completed more

--- exercise6.rb
+++ exercise6.rb
@@ -1,2 +1,21 @@
-grocery_list= ["dogs", "cats", "birds", "fishes"].sort
-p grocery_list
+grocery_list= ["dogs", "cats", "birds", "fishes", "salmon"].sort
+def print_list(list)
+	def iteratelist(list)
+    	list.each do |i|
+	   		puts "* #{i}"
+    	end
+    	puts "- - -"
+    end
+    iteratelist(list)
+    puts "Number of things: #{list.size}"
+    puts list.include?("banana") ? "You don't need bananas today" : "Pick up bananas"
+    puts "Second item #{list[1]}"
+    list.delete("salmon")
+    iteratelist(list)
+    
+end
+
+print_list(grocery_list)
+grocery_list << "rice"
+print_list(grocery_list)
+
